83cdce4f3758f60f3294ce5afbbfdaf4fd653ab3
Michael Amar <michaam3@ac.sce.ac.il>
2018-12-17 00:59:11 +0200

    Just a little change

--- Sniffer.py
+++ Sniffer.py
@@ -5,9 +5,7 @@ counter = 0
 def sniffAction(pkt):
     global counter
     counter += 1
-    if counter%2==0:
-    	return
     return'{} ==> {} || Size : {} bytes || Data : {}'.format(pkt[0][1].src, pkt[0][1].dst, pkt[0][1].len, pkt[0][3].load)
 
 
-sniff(iface="lo0", filter="udp port 12321", prn = sniffAction, store=0)
+sniff(filter="dst host 172.20.10.4 and port 12321 and udp", prn = sniffAction, store=0)
